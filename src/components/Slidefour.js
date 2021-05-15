6926e9dd4733aa82e1b1f5204a4dbaa0e4a99351
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-05-15 20:47:59 +0530

    articles section added

--- src/components/Slidefour.js
+++ src/components/Slidefour.js
@@ -5,9 +5,282 @@ export const Slidefour = () => {
 
     return (
         <div>
+
+
             <div className="mark4">
-                
+                <div className="m4h">KNOWLEDGE</div>
+                <div className="m4hs">ARTICLES</div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm">
+                        <div className="article-section">#Trending
+                                <a className="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>  
+                            </div>
+                            <div id="carouselExampleControls1" className="carousel slide" data-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="article-card">
+                                            <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                                Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU" alt="" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                         
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                        <div className="article-section">#Latest
+                                <a className="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>  
+                            </div>
+                            <div id="carouselExampleControls2" className="carousel slide" data-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="article-card">
+                                            <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                                Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU" alt=""/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU" alt=""/>
+                                            </div>
+                                         
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                        <div className="article-section">#Popular
+                                <a className="carousel-control-prev" href="#carouselExampleControls3" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleControls3" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>  
+                            </div>
+                            <div id="carouselExampleControls3" className="carousel slide" data-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="article-card">
+                                            <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                                Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="article-card">
+                                        <div className="article-date">
+                                                15th May, 2021
+                                                
+                                              <span className="article-stats">4028 <i class="fas fa-heart"></i> 256 <i class='fas fa-comment'></i></span>
+                                            
+                                            </div>
+                                            <div className="article-title">
+                                                This is my first article on codespark!
+                                            </div>
+                                            <div className="article-intro">
+                                                lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.lorem ipsum doloe sit amet.
+                                                lorem ipsum doloe sit amet.
+                                            </div>
+                                            <div className="article-readmore-btn">
+                                            Read more...
+                                            </div>
+                                            <div className="article-image">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaKuLgsel-1tFC1myiPqhiwVm883cpYdiEgg&usqp=CAU"alt="" />
+                                            </div>
+                                         
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                                </div>
+                            </div>
+                    </div>
+
+        </div>
     )
 }
